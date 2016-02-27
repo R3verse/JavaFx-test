53f561c7a33b39e2ce7553b6c10c1e379ffc26e7
Max Jensen <Max@maxjensen.dk>
2016-02-27 13:25:08 +0100

    Table view done

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,61 @@
-/**
- * Created by Maxjensendk on 27-02-2016.
- */
-
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private Stage window;
-    private Button button;
-
+    Stage window;
+    TableView<Product> table;
 
     public static void main(String[] args) {
-
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        window.setTitle("thenewboston");
-        button = new Button("Click Me");
+        window.setTitle("Product store - Max Jensen");
 
-        button.setOnAction(e -> {
-            boolean result = ConfirmBox.display("Title of window", "Are you sure?!");
-            System.out.println(result);
-        });
+       TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
+        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(200);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(200);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(table);
+
+        Scene scene = new Scene(vbox);
         window.setScene(scene);
         window.show();
-
     }
+
+    public ObservableList<Product> getProduct()
+    {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Milk", 15.00, 20));
+        products.add(new Product("Eggs", 30.00, 20));
+        products.add(new Product("Tuna", 10.00, 50));
+
+
+        return products;
+    }
+
 }
