558a4944ed42f6f8869bbda2da366bc10dd81508
Max Jensen <Max@maxjensen.dk>
2016-02-27 11:42:24 +0100

    Event handler test button

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@
  */
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
 
     private Button button;
     public static void main(String[] args) {
@@ -22,6 +24,7 @@ public class Main extends Application {
 
         button = new Button();
         button.setText("Click me");
+        button.setOnAction(this);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -29,5 +32,13 @@ public class Main extends Application {
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == button)
+        {
+            System.out.println("Clicked!");
+        }
     }
 }
