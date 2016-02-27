2777d1cc04bd34f31fb4d409e86c3d31fc9d4b9e
Max Jensen <Max@maxjensen.dk>
2016-02-27 12:03:55 +0100

    Adding second scene and btn to switch between the 2

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,47 @@
  */
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent>{
+public class Main extends Application {
 
-    private Button button;
+    private Stage window;
+    private Scene scene1;
+    private Scene scene2;
+
     public static void main(String[] args) {
 
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Title of the window");
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
 
-        button = new Button();
-        button.setText("Click me");
-        button.setOnAction(this);
+        Label label1 = new Label("Welcome to first scene");
+        Button btn1 = new Button("Go to scene 2");
+        btn1.setOnAction(e -> window.setScene(scene2));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        // layout 1 - vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, btn1);
+        scene1 = new Scene(layout1, 350, 500);
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Button btn2 = new Button("GO back to scene 1");
+        btn2.setOnAction(e -> window.setScene(scene1));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(btn2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Title of scene");
+        window.show();
     }
 
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource() == button)
-        {
-            System.out.println("Clicked!");
-        }
-    }
 }
